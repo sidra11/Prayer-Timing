6833e1482c3b4a416fde6584ab12b614d1375e47
sidra11 <sidra.peace2010@hotmail.com>
2023-11-20 22:42:25 +1300

    add the location
    (cherry picked from commit 47fb042b5581e90c2182b31f33b3095e79920359)

--- App.js
+++ App.js
@@ -1,29 +1,47 @@
-import { View, StyleSheet,ImageBackground } from 'react-native';
-import {LinearGradient} from 'expo-linear-gradient';
-import MainScreen from './screen/MainScreen';
-import TopScreen from './screen/TopSceen';
-import BottomScreen from './screen/BottomScreen';
+import {
+  Text,
+  View,
+  StyleSheet,
+  ImageBackground,
+  SafeAreaView,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import React, { useState, useEffect } from "react";
+import TopScreen from "./screen/TopSceen";
+import BottomScreen from "./screen/BottomScreen";
+import LocationScreen from "./screen/LocationScreen";
+// import PrayerTimesComponent from "./components/PrayerTimes"
+
+
 
 export default function App() {
-  return (
-    
-    <LinearGradient colors={['#1c541c', '#ddb52f']} style={styles.rootScreen}>
   
-      <ImageBackground source={require('./assets/images/background.jpg')} 
-      resizeMode="cover" style={styles.rootScreen} imageStyle={styles.backgroundImage}>
-        <TopScreen/>
-      <MainScreen/>
-     <BottomScreen/>
+
+
+  return (
+    <LinearGradient colors={["#1c541c", "#ddb52f"]} style={styles.rootScreen}>
+      <ImageBackground
+        source={require("./assets/images/background.jpg")}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <SafeAreaView style={styles.rootScreen}>
+          <TopScreen />
+          <LocationScreen/>
+       
+          <BottomScreen />
+      
+        </SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   );
 }
-   const styles = StyleSheet.create({
-    rootScreen: {
-      flex: 1,
-    },
-    backgroundImage:{
-      opacity:0.18,
-    }
-
-   });
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.18,
+  },
+});
